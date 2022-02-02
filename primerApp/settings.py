836880f9b224b18730b1803e72590b345fa3a9ea
203431 <203431@ids.upchiapas.edu.mx>
2022-02-02 12:33:16 -0600

    python-dotenv added

--- primerApp/settings.py
+++ primerApp/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
