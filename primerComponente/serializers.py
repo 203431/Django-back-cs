4ed9ec30557ebe65c0064ad921b59424df4d246c
203431 <2034341@ids.upchiapas.edu.mx>
2022-02-07 11:18:39 -0600

    add Dotenv

---
+++ primerComponente/serializers.py
@@ -0,0 +1,11 @@
+from dataclasses import field, fields
+from pyexpat import model
+from rest_framework import routers, serializers, viewsets
+
+# importacion de modelos
+from primerComponente.models import PrimerTabla
+
+class PrimerTablaSerializers(serializers.ModelSerializer):
+    class Meta: 
+        model = PrimerTabla
+        fields = ('__all__')
