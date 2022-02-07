9d368b4dc7a3c8feeac2de39198aaafad555c1b9
203431 <2034341@ids.upchiapas.edu.mx>
2022-02-07 14:56:31 -0600

    responseOk added

--- primerComponente/views.py
+++ primerComponente/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from multiprocessing import context
 from rest_framework import status
+# Importación json
+import json
+
 
 # Importaciones de modelo
 from primerComponente.models import PrimerTabla
@@ -13,11 +16,18 @@ from primerComponente.models import PrimerTabla
 from primerComponente.serializers import PrimerTablaSerializers
 
 # Create your views here.
+
+responseOk = '{"messages":"success", "payload": "serializer.data","status":"status"}'
+responseOk = json.loads(responseOk)
+
+
+
+
 class PrimerTablaList(APIView):
     def get(self, request, format=None):
         queryset = PrimerTabla.objects.all()
         serializers = PrimerTablaSerializers(queryset, many=True, context={'request': request})
-        return Response(serializers.data)
+        return Response(responseOk)
         
     def post(self, request, format=None):
         serializer = PrimerTablaSerializers(data = request.data)
@@ -27,6 +37,19 @@ class PrimerTablaList(APIView):
             return Response(datas, status= status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def jsonMaker(self, message, data, status):
+        json1 = {"message":message, "pay_load":data, "status":status }
+        dumper=json.dumps(json1)
+        responseOk = json.loads(dumper)
+        return responseOk
+
+
+    def get(self, request, format=None):
+        queryset = PrimerTabla.objects.all()
+        serializer = PrimerTablaSerializers(queryset, many = True, context = {'request':request})
+        responseOk = self.jsonMaker("succes", serializer.data, status.HTTP_200_OK)
+        return Response(responseOk)
+
 class PrimerTablaDetail(APIView):
     def get_objet(self, pk):
         try: 
@@ -35,6 +58,7 @@ class PrimerTablaDetail(APIView):
             return 0
 
     def get(self, request, pk, format=None):
+        print(pk)
         idResponse = self.get_objet(pk)
         if idResponse != 0:
             idResponse = PrimerTablaSerializers(idResponse)
