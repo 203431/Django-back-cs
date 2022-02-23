96f9226df20c5369e2ef3e1bc8ce791345dcf3bb
203431 <2034341@ids.upchiapas.edu.mx>
2022-02-23 13:38:45 -0600

    Register without token

--- Register/views.py
+++ Register/views.py
@@ -1,4 +1,4 @@
-from rest_framework.response import Response
+'''from rest_framework.response import Response
 from .serializers import UserSerializer
 from rest_framework.views import APIView
 from rest_framework import status
@@ -11,5 +11,14 @@ class UserAPI(APIView):
             user = serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED )
         else:
-                return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+                return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)'''
 
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
+class UserAPI(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
